53eb7e66da865d7e83e3460b56f89fafe56c44cc
Kurt Kerns <kurtkerns20@gmail.com>
2022-02-24 20:06:53 -0600

    solution for easy 1 10

--- easy1/10.rb
+++ easy1/10.rb
@@ -1,7 +1,7 @@
 =begin
 Problem
 ------------------------------------------
-calculate
+cal
 
 Inputs: 
 Outputs: 
@@ -21,3 +21,11 @@ Data Structure, Algorithm
 
 
 =end
+
+def calculate_bonus(salary, bool)
+  bool ? salary / 2 : 0
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
