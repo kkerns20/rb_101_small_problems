1dedd6a6a8f0abddd429949734ba188d0b3ccd53
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-03 13:42:05 -0600

    Solution for finding largest sentence

---
+++ medium2/1.rb
@@ -0,0 +1,67 @@
+=begin
+Problem
+------------------------------------------
+Read a text file and print the longest sentence based on words.
+
+Inputs: 1 String of sentences
+Outputs: 1 Integer
+
+Rules/Requirements
+- sentences end with either '.!?'
+
+Clarifying Questions
+- 
+
+Examples, Test Cases
+------------------------------------------
+
+
+Data Structure, Algorithm
+------------------------------------------
+- split the string into sentences
+- Initialize largest to return value of max_by when we split sentences and find length
+- Initialize length to the split of the largest and find its size
+- Use string interpolation to output the sentence length and the sentence itself
+
+=end
+
+def longest_sentence(string)
+  sentences = string.split(/[.!?]/) 
+  largest = sentences.max_by { |s| s.split.size }
+  length = largest.split.size
+  
+  puts "The following sentence is #{length} words in length: \n\n #{largest}" 
+end
+
+
+p longest_sentence("Four score and seven years ago our fathers brought forth
+on this continent a new nation, conceived in liberty, and
+dedicated to the proposition that all men are created
+equal.
+
+Now we are engaged in a great civil war, testing whether
+that nation, or any nation so conceived and so dedicated,
+can long endure. We are met on a great battlefield of that
+war. We have come to dedicate a portion of that field, as
+a final resting place for those who here gave their lives
+that that nation might live. It is altogether fitting and
+proper that we should do this.
+
+But, in a larger sense, we can not dedicate, we can not
+consecrate, we can not hallow this ground. The brave
+men, living and dead, who struggled here, have
+consecrated it, far above our poor power to add or
+detract. The world will little note, nor long remember
+what we say here, but it can never forget what they
+did here. It is for us the living, rather, to be dedicated
+here to the unfinished work which they who fought
+here have thus far so nobly advanced. It is rather for
+us to be here dedicated to the great task remaining
+before us -- that from these honored dead we take
+increased devotion to that cause for which they gave
+the last full measure of devotion -- that we here highly
+resolve that these dead shall not have died in vain
+-- that this nation, under God, shall have a new birth
+of freedom -- and that government of the people, by
+the people, for the people, shall not perish from the
+earth.")
