ce3276a2ffbc4666bc404d5718853ba3cca541f0
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-04 14:06:26 -0600

    seeing stars solutions

--- advanced1/2.rb
+++ advanced1/2.rb
@@ -12,5 +12,33 @@ def star(grid_size)
   1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
 end
 
+# Seeing Stars
+
+def star(n)
+  puts top_half(n) + [middle(n)] + bottom_half(n)
+end
+
+def top_half(n)
+  height = (n - 1) / 2
+  height.times.each_with_object([]) do |row_index, rows|
+    separator = ' ' * (height - row_index - 1)
+    row = "*#{separator}*#{separator}*".center(n)
+    rows << row
+  end
+end
+
+def middle(n)
+  '*' * n
+end
+
+def bottom_half(n)
+  top_half(n).reverse
+end
+
+# Better: Array.new(3, '*).join(separator)
+
+star(7)
+star(9)
+
 p star(7)
-p star(10)
+p star(11)
