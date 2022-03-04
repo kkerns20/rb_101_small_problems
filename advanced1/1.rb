97e8956aabe8e6f201d015b4876b94032b63d384
Kurt Kerns <84055816+kkerns20@users.noreply.github.com>
2022-03-04 13:28:29 -0600

    madlibs revisited solutions

---
+++ advanced1/1.rb
@@ -0,0 +1,25 @@
+ADJECTIVES = %w(quick lazy sleepy ugly).freeze
+NOUNS      = %w(fox dog head leg cat tail).freeze
+VERBS      = %w(spins bites licks hurdles).freeze
+ADVERBS    = %w(easily lazily noisly excitedly).freeze
+
+File.open('madlibs.txt') do |file|
+  file.each do |line|
+    puts format(line, noun:      NOUNS.sample,
+                      verb:      VERBS.sample,
+                      adjective: ADJECTIVES.sample,
+                      adverb:    ADVERBS.sample)
+  end
+end
+
+REPLACEMENTS = {
+  adjective: %w(quick lazy sleepy ugly),
+  noun:      %w(fox dog head leg),
+  verb:      %w(jumps lifts bites licks),
+  adverb:    %w(easily lazily noisily excitedly),
+}
+
+template_text = File.read('madlibs_revisited.txt')
+mad_text = template_text.gsub(/%{([a-z]*)}/) do |_|
+  REPLACEMENTS[$1.to_sym].sample
+end
